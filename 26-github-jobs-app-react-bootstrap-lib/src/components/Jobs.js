494875f64f234c7f52b73dd11712778a8b24a9c1
therahulmidha <rahul.midha31@gmail.com>
2022-06-16 22:33:12 +0530

    Added 26th app

---
+++ 26-github-jobs-app-react-bootstrap-lib/src/components/Jobs.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import { Badge, Button, Card, Collapse } from 'react-bootstrap';
+import ReactMarkdown from "react-markdown";
+import companyLogo from "../google.png";
+export const Jobs = ({ job }) => {
+    const [open, setOpen] = useState(false);
+
+    return (
+        <Card className='mb-2'>
+            <Card.Body>
+                <div className='d-flex justify-content-between'>
+                    <div>
+                        <Card.Title>
+                            {job.title} - <span className='text-muted font-weight-light'>{job.company}</span>
+                        </Card.Title>
+                        <Card.Subtitle>
+                            {new Date(job.created_at).toLocaleDateString()}
+                        </Card.Subtitle>
+                        <Badge variant="primary" className='mr-2'>{job.type}</Badge>
+                        {" "}<Badge variant="success">{job.location}</Badge>
+                        <div style={{ wordBreak: "break-all" }}>
+                            <ReactMarkdown children={job.how_to_apply} />
+                        </div>
+                    </div>
+                    <img alt={job.company} src={companyLogo} className='d-none d-md-block' height="50" />
+                </div>
+                <Card.Text>
+                    <Button variant='success' onClick={() => setOpen(prevOpen => !prevOpen)}>
+                        {!open ? "View Details": "Hide Details"}
+                        </Button>
+                </Card.Text>
+                <Collapse in={open}>
+                    <div className='mt-4'>
+                        <ReactMarkdown children={job.description} />
+                    </div>
+                </Collapse>
+            </Card.Body>
+        </Card>
+    )
+}
